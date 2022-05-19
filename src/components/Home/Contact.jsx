9d5abad509a3f0ab489b6c5a36bd65329d33010f
Aditya Mall <contact.adityamall@gmail.com>
2022-05-19 22:43:56 +0530

    made changes to width and colspan

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section
       id="#contact"
-      className="flex flex-col items-start font-poppins py-14 md:items-center lg:py-20"
+      className="flex flex-col items-start font-poppins py-14 md:items-center md:space-y-20 lg:py-20"
     >
       <div className="flex flex-col items-start space-y-2 md:items-center">
         <h1 className="text-2xl font-bold capitalize py-4 lg:py-0 lg:text-4xl">
@@ -19,7 +19,7 @@ const Contact = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 gap-y-6 md:gap-y-0 md:grid-cols-2 md:gap-x-5">
+      <div className="grid grid-cols-1 gap-y-6 md:gap-y-0 md:grid-cols-3 md:gap-x-5 lg:grid-cols-4">
         <form className="text-black w-full space-y-2 p-3 rounded-xl bg-colorOne/25 lg:w-[20vw]">
           <input
             type="text"
@@ -46,7 +46,7 @@ const Contact = () => {
           />
           <button className="p-2 bg-colorOne rounded-xl">get quote</button>
         </form>
-        <div className="p-2 w-full rounded-xl bg-colorOne/40 md:h-full lg:h-full">
+        <div className="p-2 w-full rounded-xl bg-colorOne/40 md:col-span-2 lg:h-full lg:col-span-3">
           <iframe
             title="office"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.175440722638!2d73.03357161529401!3d19.011989358920967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3abc6acf4d1%3A0xe4146edc9efa0501!2sSiddhi%20Vinayak%20Logistics%20LTD.!5e0!3m2!1sen!2sin!4v1652952561594!5m2!1sen!2sin"
