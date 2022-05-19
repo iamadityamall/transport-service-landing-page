eee228b3dbdf8c3bb634b1ece1c64875c43eb41c
Aditya Mall <contact.adityamall@gmail.com>
2022-05-19 21:59:18 +0530

    corrected the mobile version

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -18,35 +18,42 @@ const Contact = () => {
           directly visit our office
         </p>
       </div>
-      <div className="flex flex-col space-y-10 md:flex-row md:justify-between md:space-y-0 md:items-start md:mt-10 lg:mt-0 lg:grid lg:grid-cols-2 lg:gap- lg:py-16">
-        <div className="flex flex-col py-10 items-start md:py-0 w-full lg:grid lg:w-[30vw] ">
-          <form className="text-black w-full flex flex-col space-y-2 p-3 rounded-xl bg-colorOne/25">
-            <input
-              type="text"
-              placeholder="full name"
-              className="p-2 rounded-t-xl"
-            />
-            <input type="text" placeholder="email@email.com" className="p-2" />
-            <input type="tel" placeholder="contact number" className="p-2" />
-            <input type="text" placeholder="company" className="p-2 " />
-            <textarea
-              name=""
-              id=""
-              cols="30"
-              rows="10"
-              className="rounded-b-xl"
-            />
-            <button className="p-2 bg-colorOne rounded-xl">get quote</button>
-          </form>
-        </div>
-        <div className="p-2 rounded-xl bg-colorOne/40 lg:w-[40vw] lg:h-full">
+
+      <div className="grid grid-cols-1 gap-y-6 md:gap-y-0 md:grid-cols-2 md:gap-x-5">
+        <form className="text-black w-full space-y-2 p-3 rounded-xl bg-colorOne/25 lg:w-[20vw]">
+          <input
+            type="text"
+            placeholder="full name"
+            className="p-2 rounded-t-xl w-full"
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            className="p-2 w-full"
+          />
+          <input
+            type="tel"
+            placeholder="contact number"
+            className="p-2 w-full"
+          />
+          <input type="text" placeholder="company" className="p-2 w-full" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            className="rounded-b-xl w-full"
+          />
+          <button className="p-2 bg-colorOne rounded-xl">get quote</button>
+        </form>
+        <div className="p-2 w-full rounded-xl bg-colorOne/40 md:h-full lg:h-full">
           <iframe
             title="office"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.175440722638!2d73.03357161529401!3d19.011989358920967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3abc6acf4d1%3A0xe4146edc9efa0501!2sSiddhi%20Vinayak%20Logistics%20LTD.!5e0!3m2!1sen!2sin!4v1652952561594!5m2!1sen!2sin"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
-            className="w-full h-60 rounded-xl lg:h-full"
+            className="w-full h-64 md:h-full rounded-xl lg:h-full"
           ></iframe>
         </div>
       </div>
