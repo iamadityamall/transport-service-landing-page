06c213dc6c00ade87d7c87d8766f294c7c2b213b
Aditya Mall <contact.adityamall@gmail.com>
2022-05-19 21:27:39 +0530

    text-justified

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -8,18 +8,31 @@ const About = () => {
       className="font-poppins flex flex-col space-y-3 py-10 md:items-center md:py-10 md:space-y-10 lg:h-screen lg:justify-center"
     >
       <div>
-        <h1 className="text-2xl py-4 font-bold capitalize md:text-2xl lg:text-4xl">about us</h1>
+        <h1 className="text-2xl py-4 font-bold capitalize md:text-2xl lg:text-4xl">
+          about us
+        </h1>
       </div>
-      <div className="flex flex-col space-y-5 md:flex-row md:items-start md:justify-between md:space-y-0 md:space-x-5 lg:grid lg:grid-cols-2 lg:md:space-x-0 lg:gap-x-2 lg:items-center
-      ">
-        <figure className=" bg-blue-300 rounded-xl overflow-hidden lg:h-full">
-          <img src="./images/delivery.png" alt="company-logo" className="h-[30vh] w-[100vw] object-cover lg:h-full"/>
+      <div
+        className="flex flex-col space-y-5 md:flex-row md:items-start md:justify-between md:space-y-0 md:space-x-5 lg:grid lg:grid-cols-2 lg:md:space-x-0 lg:gap-x-2 lg:items-center
+      "
+      >
+        <figure className="  rounded-xl overflow-hidden lg:h-full lg:p-2">
+          <img
+            src="./images/delivery.png"
+            alt="company-logo"
+            className="h-[30vh] bg-blue-300 object-cover lg:h-full lg:w-full lg:rounded-xl"
+          />
         </figure>
-        <div className="flex flex-col md:text-right md:items-end md:space-y-3 lg:p-10
-        ">
+        <div
+          className="flex flex-col md:text-right md:items-end md:space-y-3 lg:p-10
+        "
+        >
           {data.map((item) => {
             return (
-              <p key={item.id} className="py-2 leading-relaxed text-sm text-left md:py-0 md:text-right lg:text-lg font-poppins lg:text-left" >
+              <p
+                key={item.id}
+                className="py-2 leading-relaxed text-sm text-left md:py-0 md:text-right lg:text-lg font-poppins lg:text-justify"
+              >
                 {item.info}
               </p>
             );
