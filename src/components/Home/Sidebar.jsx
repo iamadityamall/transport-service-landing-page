2684b36cefd78a851f61915338fd708f269b08bc
Aditya Mall <contact.adityamall@gmail.com>
2022-05-19 09:04:04 +0530

    reduced sidebar nav links section

--- src/components/Home/Sidebar.jsx
+++ src/components/Home/Sidebar.jsx
@@ -9,12 +9,12 @@ const Sidebar = ({ setNavButton, navButton }) => {
   const { darkmode } = useGlobalContext();
   return (
     <aside
-      className={`fixed text-black top-0 right-0 h-screen space-y-20 w-screen p-4 transition all delay-300 ease-in-out flex flex-col items-end text-right capitalize lg:hidden bg-white ${
+      className={`fixed text-black top-0 right-0 h-screen space-y-20 w-screen p-8 transition all delay-300 ease-in-out flex flex-col items-end text-right capitalize lg:hidden bg-white ${
         !navButton && "translate-x-full"
       } ${!darkmode && "bg-colorTwo text-white"}
       }`}
     >
-      <button onClick={() => setNavButton(false)} className="mt-2">
+      <button onClick={() => setNavButton(false)} className="mt-3">
         <IoClose className="text-5xl p-2 text-colorOne" />
       </button>
       <div className="flex flex-col w-full items-end">
