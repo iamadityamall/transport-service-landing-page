7d6164a8884c2e68acb901124f91770dabb4fa54
Aditya Mall <contact.adityamall@gmail.com>
2022-05-19 23:10:09 +0530

    added link to form

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -20,7 +20,11 @@ const Contact = () => {
       </div>
 
       <div className="grid grid-cols-1 gap-y-6 md:gap-y-0 md:grid-cols-3 md:gap-x-5 lg:grid-cols-4">
-        <form className="text-black w-full space-y-2 p-4 rounded-xl bg-colorOne/25 lg:w-[20vw]" action="https://formsubmit.co/addasjii@gmail.com" method="POST">
+        <form
+          className="text-black w-full space-y-2 p-4 rounded-xl bg-colorOne/25 lg:w-[20vw]"
+          action="https://formsubmit.co/addasjii@gmail.com"
+          method="POST"
+        >
           <input
             type="text"
             placeholder="full name"
@@ -47,7 +51,9 @@ const Contact = () => {
             rows="10"
             className="rounded-b-xl w-full"
           />
-          <button className="p-2 bg-colorOne rounded-xl" type="submit">get quote</button>
+          <button className="p-2 bg-colorOne rounded-xl" type="submit">
+            get quote
+          </button>
         </form>
         <div className="p-4 w-full rounded-xl bg-colorOne/40 md:col-span-2 lg:h-full lg:col-span-3">
           <iframe
